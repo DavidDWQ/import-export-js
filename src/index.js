469bf3ee0dae7d26e000f475a9b63f7196e6502b
DavidDWQ <dengwenqi4@126.com>
2019-10-06 19:13:15 +0800

    lodash

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ var calcArea = require('./squareFunction')
 var addFunction = require('./addFunction')
 var number = require('./number')
 var stats = require('simple-statistics')
+var _ = require('lodash')
 
 console.log(calcArea(number))
 console.log(addFunction(16,33))
 
 
 var numbers = [1,2,4,7,9,11,45]
+var string = 'hello, my name is David.'
 
 
 console.log(stats.max(numbers))
 console.log(stats.min(numbers))
 console.log(stats.median(numbers))
 console.log(stats.product(numbers))
-console.log(stats.(numbers))
+console.log(_.reverse(numbers))
+console.log(_.escape(string))
+console.log(_.camelCase(string))
+console.log(_.kebabCase(string))
